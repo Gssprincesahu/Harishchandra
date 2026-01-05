1b2296a7707e384b415b3e48618b49a980e3b994
Priya Singh <singhshruti4131@gmail.com>
2026-01-05 20:20:36 +0530

    Hero

--- frontend/src/WebPages/HomePage/HomePage.jsx
+++ frontend/src/WebPages/HomePage/HomePage.jsx
@@ -2,15 +2,8 @@ import Herosection from "./Herosection";
 
 export default function HomePage() {
   return (
-<<<<<<< HEAD
-    <div className="mt-10">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
-    </div>
-=======
-    <>
+    <>    
     <Herosection />
     </>
->>>>>>> 7f4a1ee902e65f179a68362fbb8576ecd9cde24d
   );
 }
