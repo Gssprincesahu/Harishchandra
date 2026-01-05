c07b395b81c3d3aa9bc09a653f9c59fe6db0a2df
Priya Singh <singhshruti4131@gmail.com>
2026-01-05 18:16:48 +0530

    education page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Router } from "react-router-dom";
 import HomePage from "./WebPages/HomePage/HomePage.jsx";
+import EducationPage from "./WebPages/EducationPage/EducationPage.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/education" element={<EducationPage />} />
     </Routes>
   );
 }
---
+++ frontend/src/WebPages/EducationPage/EducationPage.jsx
@@ -0,0 +1,8 @@
+export default function EducationPage() {
+  return (
+    <div>
+      <h1>Welcome to the Education Page</h1>
+      <p>This is the education page of the application.</p>
+    </div>
+  );
+}
